c38b4a71c54a1ceb8729b906c9a1df609fc788f7
Linux System <ericzxia@gmail.com>
2018-02-15 12:29:40 -0800

    Added comments, took things out to clean up

--- LogisticRegression.py
+++ LogisticRegression.py
@@ -2,16 +2,17 @@ import numpy as np
 import pandas as pd
 import sklearn.linear_model as sk
 
-data = pd.read_csv("train.csv")
-results = data[["toxic", "severe_toxic", "obscene", "threat", "insult", "identity_hate"]]
-text = data["comment_text"]
-categories = list(results)
+data = pd.read_csv("train.csv") #reads in data
+results = data[["toxic", "severe_toxic", "obscene", "threat", "insult", "identity_hate"]] #the outcomes
+text = data["comment_text"] #stores in the actual text
+categories = list(results) #takes the categories above and stores it in list
 #print(categories)
 
 d = []
-length = len(text) // 10
+length = len(text)
 results = results[:length]
 
+# DOES NECESSARY PARSING AND CONVERTING TEXT TO NUMERICAL DATA (Bag of Words)
 for i in range(length):
     words = text[i]
     content = words.split()
@@ -21,31 +22,16 @@ for i in range(length):
 
 df = pd.DataFrame(d).fillna(0)._get_numeric_data()
 
+# SPLITS DATASET TO TRAINING SET AND VALIDATION SET
 keep = np.random.rand(len(df)) < 0.8
 X_train = df[keep]
 Y_train = results[keep]
 X_valid = df[~keep]
 Y_valid = results[~keep]
 
-toxic_train = Y_train["toxic"]
-severe_toxic_train = Y_train["severe_toxic"]
-obscene_train = Y_train["obscene"]
-threat_train = Y_train["threat"]
-insult_train = Y_train["insult"]
-identity_hate_train = Y_train["identity_hate"]
-
-toxic_valid = Y_valid["toxic"]
-severe_toxic_valid = Y_valid["severe_toxic"]
-obscene_valid = Y_valid["obscene"]
-threat_test = Y_valid["threat"]
-insult_test = Y_valid["insult"]
-identity_hate_test = Y_valid["identity_hate"]
-
-
-
 print("USING LOGISTIC REGRESSION:")
 
-### TOXIC CASE
+# TRAINING AND EVALUATING ON VALIDATION SET
 for cat in categories:
     log_reg_toxic = sk.LogisticRegression()
     log_reg_toxic.fit(X_train, Y_train[cat])
