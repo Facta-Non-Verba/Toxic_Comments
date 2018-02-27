aee10d5923d1fe4a9fe9fcceb0a5e5a181174cff
Linux System <ericzxia@gmail.com>
2018-02-26 22:32:50 -0800

    tried figuring out how to deal with memory, if all else fails try school machines

--- LogisticRegression.py
+++ LogisticRegression.py
@@ -3,25 +3,28 @@ import pandas as pd
 import sklearn.linear_model as sk
 from sklearn.model_selection import GridSearchCV
 
-# data = pd.read_csv("train.csv") #reads in data
-# results = data[["toxic", "severe_toxic", "obscene", "threat", "insult", "identity_hate"]] #the outcomes
-# text = data["comment_text"] #stores in the actual text
-# categories = list(results)  #takes the categories above and stores it in list
+data = pd.read_csv("train.csv") #reads in data
+results = data[["toxic", "severe_toxic", "obscene", "threat", "insult", "identity_hate"]] #the outcomes
+text = data["comment_text"] #stores in the actual text
+categories = list(results)  #takes the categories above and stores it in list
 #print(categories)
 
 d = []
-length = len(text)
+length = len(text) // 10
 results = results[:length]
 
 # DOES NECESSARY PARSING AND CONVERTING TEXT TO NUMERICAL DATA (Bag of Words)
 for i in range(length):
     words = text[i]
     content = words.split()
-    counts = {x:words.count(x) for x in content}
+    counts = {x:words.count(x) for x in set(content)}
     counts['id'] = data["id"][i]
-    d.append(counts)
+    d = d + [counts]
+    counts = None
+    content = None
+    words = None
 
-df = pd.DataFrame(d)
+df = pd.DataFrame(d).fillna(0).drop(["id"])
 
 # SPLITS DATASET TO TRAINING SET AND VALIDATION SET
 keep = np.random.rand(len(df)) < 0.8
@@ -31,14 +34,15 @@ X_valid = df[~keep]
 Y_valid = results[~keep]
 
 def multi_evaluate():
-	print("USING LOGISTIC REGRESSION:")
+    print("USING LOGISTIC REGRESSION:")
 
 	# TRAINING AND EVALUATING ON VALIDATION SET
-	for cat in categories:
-	    log_reg_toxic = sk.LogisticRegression()
-	    log_reg_toxic.fit(X_train, Y_train[cat])
-	    valid_score = log_reg_toxic.score(X_valid, Y_valid[cat])
-	    print("The validation error is:", valid_score)
+    for cat in categories:
+        log_reg_toxic = sk.LogisticRegression()
+        log_reg_toxic.fit(X_train, Y_train[cat])
+        valid_score = log_reg_toxic.score(X_valid, Y_valid[cat])
+        print("For category", cat)
+        print("The validation error is:", valid_score)
 
 
 def multi_validation():
